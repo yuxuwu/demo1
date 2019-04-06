ba271e7ee369d35525192eb0c6b08259904faad6
yuxuwu <ywu148@binghamton.edu>
2019-04-06 16:27:41 -0400

    Removed libRed, fixed scoping on fragSource

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <libRed/red.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -12,78 +11,69 @@
 
 const std::string readFromFile(const std::string &pathToFile);
 const std::string readFromFile(const std::string &pathToFile) {
-    std::string content;
-    std::ifstream fileStream(pathToFile, std::ios::in);
+	std::string content;
+	std::ifstream fileStream(pathToFile, std::ios::in);
 
-    if(!fileStream.is_open()) {
-        std::cerr << "Could not read file " << pathToFile << ". File does not exist." << std::endl;
-        return "";
-    }
+	if(!fileStream.is_open()) {
+		std::cerr << "Could not read file " << pathToFile << ". File does not exist." << std::endl;
+		return "";
+	}
 
-    std::string line = "";
-    while(!fileStream.eof()) {
-        std::getline(fileStream, line);
-        content.append(line + "\n");
-    }
+	std::string line = "";
+	while(!fileStream.eof()) {
+		std::getline(fileStream, line);
+		content.append(line + "\n");
+	}
 
-    fileStream.close();
-    return content;
+	fileStream.close();
+	return content;
 }
 
 void processInput(GLFWwindow *window);
 void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}  
-
-void redGui() {
-    redOpen();
-	redPrint(redString("Starting view"));
-	redDo("view [text {Hello World!}]");
-	redPrint(redString("View started"));
-	redClose();
+	glViewport(0, 0, width, height);
 }
 
 int main() {
-  std::thread red(redGui);
-  /*
-   * Window Context setup
-   */
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	/*
+	 * Window Context setup
+	 */
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(RES_X, RES_Y, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
-	    std::cout << "Failed to create GLFW window" << std::endl;
-	    glfwTerminate();
-	    return -1;
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-	    std::cout << "Failed to initialize GLAD" << std::endl;
+		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}    
 
 	glViewport(0, 0, RES_X, RES_Y);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 
-  /*
-   * Sample scene setup
-   */
+	/*
+	 * Sample scene setup
+	 */
 	// Covers screen
 	float vertices[] = {
 		// Upper Tri
 		-1.0f,  1.0f,  0.0f, //top left corner
-		 1.0f,  1.0f,  0.0f, //top right corner
-		 1.0f, -1.0f,  0.0f, //bot right corner
+		1.0f,  1.0f,  0.0f, //top right corner
+		1.0f, -1.0f,  0.0f, //bot right corner
 		-1.0f, -1.0f,  0.0f //bot left corner
 	};
 	unsigned int indicies[] = {
@@ -110,6 +100,7 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, 0); 
 	glBindVertexArray(0); 
 
+
 	/// Shaders
 	// Vert Shader
 	const char* vertShaderSource = "#version 330 core\n layout (location = 0) in vec3 aPos;\n void main(){ gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}\0";
@@ -118,41 +109,46 @@ int main() {
 	glShaderSource(vertShader, 1, &vertShaderSource, NULL);
 	glCompileShader(vertShader);
 	int success;
-    char infoLog[512];
-    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    } else {
-	  std::cout << "VERT SHADER SUCCESS" << std::endl;
+	char infoLog[512];
+	glGetShaderiv(vertShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	} else {
+		std::cout << "VERT SHADER SUCCESS" << std::endl;
 	}
+
+
 	// Frag Shader
-	const char* fragShaderSource = readFromFile("..//rm_test.frag").c_str();
+	std::string sfragShaderSource = readFromFile("../rm_test.frag");
+	const char* fragShaderSource = sfragShaderSource.c_str();
 	unsigned int fragShader;
 	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragShader, 1, &fragShaderSource, NULL);
 	glCompileShader(fragShader);
-    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    } else {
-	  std::cout << "FRAG SHADER SUCCESS" << std::endl;
+	glGetShaderiv(fragShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	} else {
+		std::cout << "FRAG SHADER SUCCESS" << std::endl;
 	}
+
+
 	// Final Shader
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertShader);
 	glAttachShader(shaderProgram, fragShader);
 	glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    } else {
-	  std::cout << "SHADER LINKING SUCCESS" << std::endl;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	} else {
+		std::cout << "SHADER LINKING SUCCESS" << std::endl;
 	}
-	
+
 	while(!glfwWindowShouldClose(window)) {
 		processInput(window);
 
@@ -170,17 +166,17 @@ int main() {
 
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		
 
-	    glfwSwapBuffers(window);
-	    glfwPollEvents();    
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-    glDeleteShader(vertShader);
-    glDeleteShader(fragShader);
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+	glDeleteShader(vertShader);
+	glDeleteShader(fragShader);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 
 	return 0;
